144d549ddd244cd082e398b0677afd9bb97de57d
hahagarden <tingyuan413@gmail.com>
2023-05-15 14:46:04 +0900

    feat: 라우팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import MainPage from "./pages/MainPage";
+import ProductPage from "./pages/ProductPage";
+import BookmarkPage from "./pages/BookmarkPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/products/list" element={<ProductPage />} />
+        <Route path="/bookmark" element={<BookmarkPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
